0159b34c2eee9a7bc4e3a9ebc028e06b0fc169a8
Nirx90 <123075342+Nirx90@users.noreply.github.com>
2024-10-09 20:19:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-let menuList = document.getElementById("menuList")
-        menuList.style.maxHeight = "0px";
 
-        function toggleMenu(){
-            if(menuList.style.maxHeight == "0px")
-            {
-                menuList.style.maxHeight = "300px";
-            }
-            else{
-                menuList.style.maxHeight = "0px";
-            }
-        }
+
+function hidenav(){
+    const dis = document.querySelector('.hide');
+    const lela = document.querySelector('.checkbtn');
+    const cross = document.querySelector('.cross');
+    dis.style.left = '-100%';
+    lela.style.display = '';
+    cross.style.display = 'none';
+    
+}
+
+function shownav(){
+    const dis = document.querySelector('.hide');
+    const checkbtn = document.querySelector('.checkbtn');
+    const cross = document.querySelector('.cross');
+    dis.style.left = '0%';
+    checkbtn.style.display = 'none';
+    cross.style.display = 'block';
+}
